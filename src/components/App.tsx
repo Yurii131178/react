946393964274e16fdf1ed442e6dbb4041e2b6725
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-05-10 12:17:05 +0300

    lifting state up

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -435,22 +435,87 @@ return <button onClick={handleClick}>Clicked: {clicks}</button>;
 
 //Тепер в App відрендеримо три кнопки, але вони не залежать одна від одної:
 
-import ClickCounter from "./ClickCounter";
+// import ClickCounter from "./ClickCounter";
 
-export default function App() {
-  return (
-    <>
-      <div style={{marginTop: '20px', display: 'flex', gap: '20px'}}>
-        <ClickCounter />
-        <ClickCounter />
-        <ClickCounter/>
-      </div> 
-    </>
-  )
-}
+// export default function App() {
+//   return (
+//     <>
+//       <div style={{marginTop: '20px', display: 'flex', gap: '20px'}}>
+//         <ClickCounter />
+//         <ClickCounter />
+//         <ClickCounter/>
+//       </div> 
+//     </>
+//   )
+// }
 
 //Кожна кнопка має свій власний стан. Клік по одній не змінює значення в іншій.
 
 // Стан не зберігається глобально – він живе лише в компоненті, де створений.
 // У кожної копії – свій стан.
 // Компонент App не має доступу до внутрішнього стану ClickCounter.
+//...........................................................//
+
+//-----Підняття стану--------------//
+
+//Підняття стану
+// Іноді компоненти мають спільні дані, які мають бути синхронізованими. Наприклад, два або більше компонентів ClickCounter, які мають показувати одну і ту саму кількість кліків.
+// У такому випадку стан потрібно зберігати в їхньому спільному батьківському компоненті – і передавати через пропси. Це називається підняттям стану.
+
+// Проблема
+// У кожному ClickCounter є свій окремий стан:
+// Кожна кнопка має власний стан
+
+/**
+ * // src/components/ClickCounter.tsx
+ 
+export default function ClickCounter() {
+    const [clicks, setClicks] = useState(0);
+    return <button onClick={() => setClicks(clicks + 1)}>Clicked: {clicks}</button>;
+    }
+  */
+  
+
+// Результат: кнопки рахують окремо. Але що, якщо ми хочемо рахувати разом?
+
+// src/App.tsx
+/*
+import ClickCounter from "./ClickCounter";
+
+export default function App() {
+  return (
+    <>
+      <ClickCounter />
+      <ClickCounter />
+    </>
+  );
+}
+*/
+// Рішення – підняти стан
+
+// Ми переміщаємо clicks до App, і передаємо значення стану clicks як проп value, та handleClicks як проп onUpdate у ClickCounter як пропси.
+
+//Тепер обидві кнопки показують однакове значення і змінюють його синхронно.
+
+//src/components/App.tsx
+
+import { useState } from "react";
+import ClickCounter from "./ClickCounter";
+
+export default function App() {
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
+  return (
+    <>
+      <div style={{marginTop: '20px', display: 'flex', gap: '20px', paddingLeft: '40px'}}>
+        <ClickCounter value={clicks} onUpdate={handleClick} />
+        <ClickCounter value={clicks} onUpdate={handleClick} />
+        <ClickCounter value={ clicks } onUpdate={ handleClick }/>
+      </div>
+    </>
+  );
+}
--- src/components/ClickCounter.tsx
+++ src/components/ClickCounter.tsx
@@ -1,22 +1,87 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-export default function ClickCounter() {
-  const [clicks, setClicks] = useState(0);
+// export default function ClickCounter() {
+//     const [clicks, setClicks] = useState(0);
 
-  const handleClick = () => {
-    setClicks(clicks + 1);
-  };
+//     const handleClick = () => {
+//         setClicks(clicks + 1);
+//     };
 
-  return (
-    <button onClick={handleClick}>
-      CLicked:
-      {clicks}
-    </button>
-  );
-}
+//     return <button onClick={handleClick} style={{
+//       padding: '10px 20px',
+//       fontSize: '16px',
+//       backgroundColor: '#4CAF50',
+//       color: 'yellow',
+//       border: 'none',
+//       borderRadius: '5px',
+//       cursor: 'pointer'
+//     }}>CLicked:
+//         { clicks }</button>
+
+// }
 
 //Кожна кнопка має свій власний стан. Клік по одній не змінює значення в іншій.
+
+
 
 // Стан не зберігається глобально – він живе лише в компоненті, де створений.
 // У кожної копії – свій стан.
 // Компонент App не має доступу до внутрішнього стану ClickCounter.
+
+//.............................................................................//
+
+//-----Підняття стану--------------//
+
+//Підняття стану
+// Іноді компоненти мають спільні дані, які мають бути синхронізованими. Наприклад, два або більше компонентів ClickCounter, які мають показувати одну і ту саму кількість кліків.
+// У такому випадку стан потрібно зберігати в їхньому спільному батьківському компоненті – і передавати через пропси. Це називається підняттям стану.
+
+// Проблема
+// У кожному ClickCounter є свій окремий стан:
+// Кожна кнопка має власний стан
+
+/**
+ * // src/components/ClickCounter.tsx
+ 
+export default function ClickCounter() {
+    const [clicks, setClicks] = useState(0);
+    return <button onClick={() => setClicks(clicks + 1)}>Clicked: {clicks}</button>;
+    }
+  */
+  
+
+// Результат: кнопки рахують окремо. Але що, якщо ми хочемо рахувати разом?
+
+// src/App.tsx
+/*
+import ClickCounter from "./ClickCounter";
+
+export default function App() {
+  return (
+    <>
+      <ClickCounter />
+      <ClickCounter />
+    </>
+  );
+}
+*/
+// Рішення – підняти стан
+
+// Ми переміщаємо clicks до App, і передаємо значення стану clicks як проп value, та handleClicks як проп onUpdate у ClickCounter як пропси.
+
+interface ClickCounterProps {
+  value: number;
+  onUpdate: () => void;
+}
+
+export default function ClickCounter({ value, onUpdate }: ClickCounterProps) {
+  return <button onClick={onUpdate}  style={{
+      padding: '10px 20px',
+      fontSize: '16px',
+      backgroundColor: '#4CAF50',
+      color: 'yellow',
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer'
+    }}>Clicked: {value}</button>;
+}
